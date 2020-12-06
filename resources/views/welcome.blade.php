6b938815da8db89f380144d52726ff5debad2be7
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-12-06 13:59:34 +0700

    init project laravel tailwind css 2.0

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+        <div class="container mx-auto">
+            <h1 class="text-6xl">This is Build from tailwindcss</h1>
+        </div>
+
+    </body>
+</html>
